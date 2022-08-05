1ffff8c192264cea66c3f1c732cb18659fcb77ef
João P. BADAN <jpbadan@gmail.com>
2022-08-05 17:05:38 +0200

    Update misc.lua

--- wordle/misc.lua
+++ wordle/misc.lua
@@ -9,73 +9,76 @@ function _init()
     kbCol, kbRow, cursorKeyNb = 1, 1, 5
 
     testWord = ""
+	
+	
+	
+	currentWord = "HI"
+	baseline = str2word("BOSON")
+	
+	wordList = {}
 
 end
 
 function _update()
-    pressedKey = updateKeyboard()
-    if btnp(5) then
-        testWord = testWord .. pressedKey
-    elseif btnp(4) then
-        testWord = sub(testWord, 1, #testWord - 1)
-    end
+
+	-- KeyPad listener --
+    currentKey = updateKeyboard()
+	currentWord, _ = keypadListener(currentWord, currentKey)
+	
+	
+	
+	if currentKey == ">" then
+		if #currentWord == 5 then 
+			-- TODO: VALIDATE WORD (COMPARE WITH WORD LIST), THEN ADD TO LIST
+			if wordList[#wordList] != currentWord then
+				add(wordList,str2word(currentWord))
+				colorLetters(baseline,wordList[#wordList])
+				currentKey = ""
+				currentWord = ""
+			end
+		else
+			-- TODO: VIBRATE SCREEN
+		end
+	end
+	
 end
 
-function _draw() drawkeyboard() end
+function _draw() 
 
----
-
-function updateKeyboardColor(char, kolor)
-    for i = 1, #keyboard do
-        if keyboard[i].char == char then
-            keyboard[i].color = kolor
-            return keyboard
-        end
-    end
+cls()
+drawkeyboard() 
+drawboard(wordList, currentWord)
 end
 
-function getNextKey(currentKey, dir)
-    if currentKey == ">" then currentKey = "ENT" end
+--- KEY PAD ---
 
-    local directions = {
-        Q = "ZAWP",
-        W = "ZAEQ",
-        E = "XSRW",
-        R = "CDTE",
-        T = "VFYR",
-        Y = "BGUT",
-        U = "NHIY",
-        I = "MJOU",
-        O = "#KPI",
-        P = "#LQO",
-        A = "WZSL",
-        S = "EXDA",
-        D = "RCFS",
-        F = "TVGD",
-        G = "YBHF",
-        H = "UNJG",
-        J = "IMKH",
-        K = "O#LJ",
-        L = "P#AK",
-        Z = "AWX#",
-        X = "SECZ",
-        C = "DRVX",
-        V = "FTBC",
-        B = "GYNV",
-        N = "HUMB",
-        M = "JI#N",
-        ENT = "KOZN"
-    }
+function keypadListener(currentWord, currentKey)
+	-- Modifies current word according to keypad and keyboard, returns last pressed key
+	lastPressedKey = ""
+	
+	if btnp(5) then
+		if currentKey == ">" then
+			return currentWord
 
-    local index = {U = 1, D = 2, R = 3, L = 4}
+		elseif #currentWord >= 5 then -- Dont add letter
+			-- TODO: VIBRATE SCREEN
+			return currentWord
+			
+		end
+		
+		currentWord = currentWord..currentKey -- Add letter
+		
+	elseif btnp(4) then	
+		currentWord = sub(currentWord,1,#currentWord-1)
+	end
+	
+	return currentWord
 
-    nextKey = sub(directions[currentKey], index[dir], index[dir])
-    if nextKey == "#" then nextKey = ">" end
-
-    return nextKey
 end
 
-------------------------------
+
+---- KEYBOARD FUNCTIONS -----
+
 function initKeyboard()
     local defaultKeyColor = 7 -- WHITE
     local chars = {
@@ -100,23 +103,6 @@ function initKeyboard()
     -- end
 end
 
-------------------------------
-function updateKeyboard()
-    if btnp(2) then -- UP
-        currentKbKey = getNextKey(currentKbKey, "U")
-    elseif btnp(3) then -- DOWN
-        currentKbKey = getNextKey(currentKbKey, "D")
-    elseif btnp(1) then -- RIGHT
-        currentKbKey = getNextKey(currentKbKey, "R")
-    elseif btnp(0) then -- LEFT
-        currentKbKey = getNextKey(currentKbKey, "L")
-    elseif btn(5) then -- X
-        return currentKbKey
-    end
-
-end
-
-------------------------------
 function drawKeyboard()
     cls()
     cursor(0, 0)
@@ -154,120 +140,191 @@ function drawKeyboard()
     rect(1, 91, 126, 126) -- Keyboard outline
 end
 
+function updateKeyboard()
+    if btnp(2) then -- UP
+        currentKbKey = getNextKey(currentKbKey, "U")
+    elseif btnp(3) then -- DOWN
+        currentKbKey = getNextKey(currentKbKey, "D")
+    elseif btnp(1) then -- RIGHT
+        currentKbKey = getNextKey(currentKbKey, "R")
+    elseif btnp(0) then -- LEFT
+        currentKbKey = getNextKey(currentKbKey, "L")
+    elseif btn(5) then -- X
+        return currentKbKey
+    end
 
-function _init()
--- ------ word module ------
+end
+
+function updateKeyboardColor(char, kolor)
+    for i = 1, #keyboard do
+        if keyboard[i].char == char then
+            keyboard[i].color = kolor
+            return keyboard
+        end
+    end
+end
+
+function getNextKey(kbKey, dir)
+    if kbKey == ">" then kbKey = "ENT" end
+
+    local directions = {
+        Q = "ZAWP",
+        W = "ZAEQ",
+        E = "XSRW",
+        R = "CDTE",
+        T = "VFYR",
+        Y = "BGUT",
+        U = "NHIY",
+        I = "MJOU",
+        O = "#KPI",
+        P = "#LQO",
+        A = "WZSL",
+        S = "EXDA",
+        D = "RCFS",
+        F = "TVGD",
+        G = "YBHF",
+        H = "UNJG",
+        J = "IMKH",
+        K = "O#LJ",
+        L = "P#AK",
+        Z = "AWX#",
+        X = "SECZ",
+        C = "DRVX",
+        V = "FTBC",
+        B = "GYNV",
+        N = "HUMB",
+        M = "JI#N",
+        ENT = "KOZN"
+    }
+
+    local index = {U = 1, D = 2, R = 3, L = 4}
+
+    nextKey = sub(directions[kbKey], index[dir], index[dir])
+    if nextKey == "#" then nextKey = ">" end
+
+    return nextKey
+end
+
+------ WORD FUNCTIONS -------
 
 function str2word(str)
-local word = {}
+	local word = {}
 
-for i=1,#str do
-word[i] = {char=sub(str,i,i),color=7} --white
-end
+	for i=1,#str do
+		word[i] = {char=sub(str,i,i),color=7} --white
+	end
 
-return word
-end
-
--- compares word to baseline, modifies word colors to reflect answer
-function comparewords(baseline,word)
-stdcolor = 7 -- white (uncompared text color)
-
--- exact letter, exact location : colors green
-for b=1,#baseline do
-if baseline[b].char == word[b].char then
-word[b].color = 3 --green
-end
+	return word
 end
 
--- exact letter, different location: colors orange
-for b=1,#baseline do
-for w=1,#word do
-if word[w].color == stdcolor then
-if baseline[b].char == word[w].char then
-word[w].color = 9 --orange
-break
-end
-end
-end
+function colorLetters(baseline,word)
+	-- compares word to baseline, modifies word colors to reflect answer
+	stdcolor = 7 -- white (uncompared text color)
+
+	-- Exact letter, exact location : green letter
+	for b=1,#baseline do
+		if baseline[b].char == word[b].char then
+			word[b].color = 3 --green
+		end
+	end
+
+	-- Exact letter, different location: orange letter
+	for b=1,#baseline do
+		for w=1,#word do
+			if word[w].color == stdcolor then
+				if baseline[b].char == word[w].char then
+					word[w].color = 9 --orange
+					break -- prevents from coloring a second letter
+				end
+			end
+		end
+	end
+
+	-- Letter not present in baseline: grey letter
+	for w=1,#word do
+		if word[w].color == stdcolor then
+			word[w].color = 5 --grey
+		end
+	end
+
+	return word
 end
 
--- letter not in baseline: colors grey
-for w=1,#word do
-if word[w].color == stdcolor then
-word[w].color = 5 --grey
-end
-end
+function printword(word, textCursor)
+	-- Prints word according to individual letter colors
+	for i=1,#word do
+		cursor(textCursor.x,textCursor.y)
+		print(word[i].char, word[i].color)
+		textCursor.x = textCursor.x + 10
+	end
 
-return word
-end
-
-
-function printword(word, cursorx)
-for i=1,#word do
-cursor(cursorx,63)
-print(word[i].char, word[i].color)
-cursorx = cursorx + 10
-end
-
-return word, cursorx
-end
-
-
-function drawboard(words)
--- draws a 5x6 grid of squares
-
-letterSqOrig = {x=3,y=2} --px
-sqsize = 8 --px
-sqspacing = 2 --px
-boardox = 63-(sqsize*5+sqspacing*4)/2
-boardoy = 10
-
-x0, y0 = boardox, boardoy
-x1, y1 = boardox+sqsize, boardoy+sqsize
-cx, cy = x0+letterSqOrig.x, y0+letterSqOrig.y
-for j=1,6 do
-word = words[j]
-for i=1,5 do
-if word then
--- draws squares with letters
-cursor(cx,cy)
-rectfill(x0,y0,x1,y1,word[i].color)
-print(word[i].char, 7) -- White
-else
--- draws empty squares
-rect(x0,y0,x1,y1,5) --Grey
-end
--- Sets new x coordinates
-x0 = x0+sqsize+sqspacing
-x1 = x1+sqsize+sqspacing
-cx = cx+sqsize+sqspacing
-end
--- Sets new y coordinates
-y0 = y0+sqsize+sqspacing
-y1 = y1+sqsize+sqspacing
-cy = cy+sqsize+sqspacing
--- resets x
-x0 = boardox
-x1 = boardox+sqsize
-cx = x0+letterSqOrig.x
+	return word, textCursor
 end
 
 
+------ BOARD FUNCTIONS -------
+
+function drawboard(words, currentWord)
+	-- draws a 5x6 grid of squares
+	
+	-- Board Properties
+	squareSize = 8 --px
+	squareSpacing = 2 --px
+	boardOrigin = {x= 63-(squareSize*5+squareSpacing*4)/2, y= 10} -- Calculates vertical center
+	squareOrigin = {x= 3, y= 2} --px
+	
+	rect0 = {x=boardOrigin.x, y=boardOrigin.y} -- left upper corner
+	rect1 = {x=boardOrigin.x+squareSize, y=boardOrigin.y+squareSize} -- right lower corner/
+	textCursor = {x= rect0.x+squareOrigin.x, y= rect0.y+squareOrigin.y} -- Cursor coordinates
+	
+	for v=1,6 do
+		word = words[v]
+		
+		for u=1,5 do
+			cursor(textCursor.x,textCursor.y)
+			if word then -- draws squares with letters
+				-- colors rectangle with letter color:
+				rectfill(rect0.x,rect0.y,rect1.x,rect1.y,word[u].color) 
+				print(word[u].char, 7) -- White letter
+				
+			else -- draws empty squares
+				rect(rect0.x,rect0.y,rect1.x,rect1.y,5) --Grey rectangle
+
+				if v == #words + 1 then -- First empty rectangle
+					rect(rect0.x,rect0.y,rect1.x,rect1.y,6) -- White rectangle
+					print(sub(currentWord,u,u), 7) -- White letter
+				end
+				
+			end
+			
+			-- Sets new x coordinates
+			rect0.x = rect0.x+squareSize+squareSpacing
+			rect1.x = rect1.x+squareSize+squareSpacing
+			textCursor.x = textCursor.x+squareSize+squareSpacing
+			
+		end
+		
+		-- Sets new y coordinates
+		rect0.y = rect0.y+squareSize+squareSpacing
+		rect1.y = rect1.y+squareSize+squareSpacing
+		textCursor.y = textCursor.y+squareSize+squareSpacing
+		-- Resets X coordinates
+		rect0.x = boardOrigin.x
+		rect1.x = boardOrigin.x+squareSize
+		textCursor.x = rect0.x+squareOrigin.x
+		
+	end
 end
 
 
+----- MISC ------
 
 
-
-
-
-cls()
-test = str2word("BOSON")
-test2 = str2word("COLOM")
-baseline = str2word("COLMO")
-ans = comparewords(baseline,test)
-ans2 = comparewords(baseline,test2)
---printword(ans, 30)
-drawboard({ans,ans2})
-
-end
+-- cls()
+-- test = str2word("BOSON")
+-- test2 = str2word("COLOM")
+-- baseline = str2word("COLMO")
+-- ans = colorLetters(baseline,test)
+-- ans2 = colorLetters(baseline,test2)
+-- --printword(ans, 30)
+-- drawboard({ans,ans2})
